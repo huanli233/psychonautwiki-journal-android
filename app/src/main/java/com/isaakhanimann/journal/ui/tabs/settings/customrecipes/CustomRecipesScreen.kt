2911be7ce02c4ded2a7131d58687f1659f7eb469
huanli233 <392352840@qq.com>
2025-10-01 22:30:39 +0800

    fix: no navigateToCustomRecipeArchive button

--- app/src/main/java/com/isaakhanimann/journal/ui/tabs/settings/customrecipes/CustomRecipesScreen.kt
+++ app/src/main/java/com/isaakhanimann/journal/ui/tabs/settings/customrecipes/CustomRecipesScreen.kt
@@ -86,7 +86,9 @@ fun CustomRecipesScreenContent(
             TopAppBar(
                 title = { Text(stringResource(R.string.custom_recipes)) },
                 actions = {
-                    
+                    IconButton(onClick = navigateToCustomRecipeArchive) {
+                        Icon(Icons.Default.Inventory, contentDescription = stringResource(R.string.archive_title))
+                    }
                 })
         },
         floatingActionButton = {
